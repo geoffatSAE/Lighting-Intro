df190fbff0ca9b5f36c5902bcbcc3c1848624a35
GeoffHill <g.hill@sae.edu>
2019-03-05 14:10:41 +1100

    initial commit from old lighting project, transferred to 2018.3

---
+++ Assets/IntensityProgram.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntensityProgram : MonoBehaviour {
+
+    public Light l;
+    float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        l = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+
+        if(timer >= 0.2)
+        {
+            l.intensity = Random.Range(0.1f, 1.0f);
+            timer = 0.0f;
+        }
+        
+
+        
+        	
+	}
+}
